38313f01cf48d87c18ef38c28c4898d0d5b1721a
Veikko Soininen <veikko.soininen@gmail.com>
2021-07-15 18:31:16 +0300

    Cleanup webpack config a bit

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,17 @@ const nodeExternals = require('webpack-node-externals');
 module.exports = {
   context: __dirname,
   entry: {
-    'dist/products/index': path.resolve(__dirname, 'products/index.js'),
-    'dist/lists/index': path.resolve(__dirname, 'lists/index.js'),
-    'dist/shared/index': path.resolve(__dirname, 'shared/index.js'),
-    'dist/preflight/index': path.resolve(__dirname, 'preflight/index.js'),
+    'dist/products': path.resolve(__dirname, 'products/index.js'),
+    'dist/lists': path.resolve(__dirname, 'lists/index.js'),
+    'dist/shared': path.resolve(__dirname, 'shared/index.js'),
+    'dist/preflight': path.resolve(__dirname, 'preflight/index.js'),
   },
   output: {
     path: __dirname,
-    filename: '[name].js',
+    filename: '[name]/index.js',
     libraryTarget: 'commonjs2',
   },
   target: 'node',
   externals: [nodeExternals()],
-  plugins: [],
   mode: 'production',
 };
