afd29582ae5efa7aa9e38622e94225b9f1e3238b
Veikko Soininen <veikko.soininen@gmail.com>
2022-01-07 15:27:00 +0200

    Add dotenv support

--- bin/lambda.ts
+++ bin/lambda.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+require('dotenv').config({silent: true})
 import "source-map-support/register"
 import cdk = require("aws-cdk-lib")
 import { LambdaApiStack } from "../lib/lambda-api-stack"
